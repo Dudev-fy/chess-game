4b9e72c4cbd17f276317ca72c9aa20d0f431085e
Dudev-fy <santos.eduardom06.03@gmail.com>
2024-08-27 20:53:54 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Board;
+
+namespace Chess {
+    class Program {
+        static void Main(string[] args) {
+            Position p;
+
+            p = new Position(3, 4);
+
+            Console.WriteLine("position " + p);
+        }
+    }
+}
---
+++ board/Position.cs
@@ -0,0 +1,15 @@
+namespace Board {
+    class Position {
+        public int line { get; set; }
+        public int column { get; set; }
+
+        public Position(int line, int column) {
+            this.column = column;
+            this.line = line;
+        }
+
+        public override string ToString() {
+            return line + "," + column;
+        }
+    }  
+}
